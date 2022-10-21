168ad21f3c3cd59338564859c2a96e1f83d3203a
YOOBINNOH <kuninyb@naver.com>
2022-10-21 17:17:36 +0900

    [ADD] Basic Controller

--- src/test/java/ItemWeb/ItemWeb/ItemWebApplicationTests.java
+++ src/test/java/ItemWeb/ItemWeb/ItemWebApplicationTests.java
@@ -1,13 +1,19 @@
 package ItemWeb.ItemWeb;
 
+import ItemWeb.ItemWeb.Member.Member;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.web.WebAppConfiguration;
 
 @SpringBootTest
+@WebAppConfiguration
 class ItemWebApplicationTests {
 
 	@Test
-	void contextLoads() {
+	void member_number() {
+
 	}
+
 
 }
