10e319e17f23996bbfdb059e115931b6ce02e692
Lee-Jiseung <ljss98@naver.com>
2022-10-23 23:02:01 +0900

    [Test] 깃 테스트

--- src/test/java/ItemWeb/ItemWeb/ItemWebApplicationTests.java
+++ src/test/java/ItemWeb/ItemWeb/ItemWebApplicationTests.java
@@ -1,7 +1,5 @@
 package ItemWeb.ItemWeb;
 
-import ItemWeb.ItemWeb.Member.Member;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.web.WebAppConfiguration;
