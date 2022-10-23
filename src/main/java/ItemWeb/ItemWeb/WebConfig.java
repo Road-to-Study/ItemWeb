bb915a230cb2ff6d7696eaa6785282963d96074e
YOOBINNOH <kuninyb@naver.com>
2022-10-23 22:59:51 +0900

    [Modify] 로그인 회원가입 html

--- src/main/java/ItemWeb/ItemWeb/WebConfig.java
+++ src/main/java/ItemWeb/ItemWeb/WebConfig.java
@@ -12,8 +12,8 @@ public class WebConfig implements WebMvcConfigurer {
     public void addInterceptors(InterceptorRegistry registry) {
         //로그인 인증 인터셉터 추가 - 홈화면, 로그인, 로그아웃, 회원가입 제외
         registry.addInterceptor(new LoginCheckInterceptor())
-                .order(1)
-                .addPathPatterns("/**")
-                .excludePathPatterns("/", "/login", "/logout", "/register", "/css/**", "/*.ico", "/error");
+                .order(1) // 인터셉터 순서
+                .addPathPatterns("/**") // 모든 url 검증
+                .excludePathPatterns("/", "/login", "/logout", "/register", "/css/**", "/*.ico", "/error"); // 예외
     }
 }
